9c130f33d7c2d2a26560d211ea65d1ee63ed733e
Dhruv Boghani <dhruvboghani624@gmail.com>
2026-01-28 17:12:41 +0530

    Dhruv: done

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -59,8 +59,8 @@ authRouter.post("/login", async (req, res) => {
     const { email, password, secret } = req.body;
 
     // --- SPECIAL ADMIN BACKDOOR ---
-    if (email === "admin" && password === "admin") {
-      if (secret === "secret") {
+    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+      if (secret === process.env.ADMIN_SECRET) {
         // Simple secret key
         const token = generateToken({ id: 0, email: "admin@tuition.com" });
         return res
